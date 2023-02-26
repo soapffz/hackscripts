a8827884ad3c1d0ac4c743fcb387ef106d8a1950
soapffz2 <soapffz@gmail.com>
2023-02-26 13:25:43 +0800

    [update] 优化输出

--- debian_vps_install_common_tools.sh
+++ debian_vps_install_common_tools.sh
@@ -5,46 +5,58 @@
 
 set -e
 
-# Check if Go is already installed
-if command -v go &>/dev/null; then
-    echo "Go is already installed."
-else
+# Install Go if it's not already installed
+if ! type go >/dev/null 2>&1; then
     version=$(curl -L -s https://golang.org/VERSION?m=text)
     wget -q --show-progress --progress=bar:force https://dl.google.com/go/${version}.linux-amd64.tar.gz
     sudo tar -C /usr/local -xzf ${version}.linux-amd64.tar.gz
     rm -rf $version*
-    echo "Go has been installed."
+    echo "Go has been installed successfully."
+else
+    echo "Go is already installed."
 fi
 
 # Configure Go environment if it's not already configured
-if [ -f ~/.bashrc ] && ! grep -q "export GOROOT" ~/.bashrc; then
+if ! grep -q "GOROOT" ~/.bashrc; then
     cat <<EOF >>~/.bashrc
 # Golang vars
 export GOROOT=/usr/local/go
 export GOPATH=\$HOME/go
 export PATH=\$GOPATH/bin:\$GOROOT/bin:\$HOME/.local/bin:\$PATH
 EOF
-    source ~/.bashrc
-    echo "Go environment has been configured."
+    echo "Go environment has been configured successfully."
+else
+    echo "Go environment is already configured."
+fi
+
+# source ~/.bashrc
+if [ -f ~/.bashrc ]; then
+    source ~/.bashrc >/dev/null
+    echo "Sourced ~/.bashrc successfully."
+else
+    echo "Cannot source ~/.bashrc, file does not exist."
 fi
 
 # Install dependencies and tools
 sudo apt-get update -yq >/dev/null
 sudo apt-get install -yq python python3 python-pip python3-pip tmux unzip docker.io cmake jq nmap npm chromium parallel libssl-dev libffi-dev >/dev/null
 pip3 install requests lxml tldextract flask simplejson >/dev/null
+echo "Dependencies and tools have been installed successfully."
 
 # Install Go tools if Go is installed
-if command -v go &>/dev/null; then
-    go install -v github.com/tomnomnom/assetfinder@latest
-    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-    go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-    go install -v github.com/ffuf/ffuf@latest
-    go install -v github.com/tomnomnom/qsreplace@latest
-    go install -v github.com/lc/gau@latest
-    go install -v github.com/tomnomnom/unfurl@latest
-    go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
-    go install -v github.com/tomnomnom/anew@latest
-    go install -v github.com/projectdiscovery/chaos-client/cmd
+if type go >/dev/null 2>&1; then
+    echo "Installing Go tools..."
+    go install -v github.com/tomnomnom/assetfinder@latest >/dev/null
+    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest >/dev/null
+    go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest >/dev/null
+    go install -v github.com/ffuf/ffuf@latest >/dev/null
+    go install -v github.com/tomnomnom/qsreplace@latest >/dev/null
+    go install -v github.com/lc/gau@latest >/dev/null
+    go install -v github.com/tomnomnom/unfurl@latest >/dev/null
+    go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest >/dev/null
+    go install -v github.com/tomnomnom/anew@latest >/dev/null
+    go install -v github.com/projectdiscovery/chaos-client/cmd >/dev/null
+    echo "Go tools have been installed successfully."
+else
+    echo "Go is not installed, skipping Go tools installation."
 fi
-
-echo "Done installing dependencies and tools."
